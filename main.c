74d42b5264ba71cee7a7a2fb8dc36daba47ccd9e
Mehdi Heidari <heidaria844@gmail.com>
2024-02-12 10:41:37 +0330

    sign in and sign up

--- main.c
+++ main.c
@@ -136,3 +136,233 @@ struct lasami* read_from_file(int *maxId)
     return phead;
 }
 
+
+struct lasami* sign_up(struct lasami* phead,int maxId){
+while(1) {
+    system("cls");
+    char name[40];
+    char pass[20];
+    char confirmpass[20];
+    char email[100];
+    char key='a';
+    int i = 0;
+    int static ID = 0;
+    struct lasami* ptemp = phead;
+    printf("enter youe user name :");
+    scanf("%s",name);
+    while(1)
+    {
+    if(strcmp(ptemp->info.username , name) == 0)
+    {
+        system("cls");
+        printf("you should change your name enter again");
+        scanf("%s",name);
+        _getch();
+        ptemp = phead;
+        continue;
+    }
+    if(ptemp->pnext)
+    {
+        ptemp = ptemp->pnext;
+        continue;
+    }
+    break;
+}
+
+    system("cls");
+    while(1)
+    {
+        printf("pleas enter your pass:\n");
+        i = 0;
+        pass[i] = '\0';
+        key='\0';
+        while(key != '\r' && key != ' ')
+        {
+            
+
+             key = _getch();
+            if(key == '\b' && i>0)
+            {
+                
+                pass[i] = '\0';    
+                printf("\b \b");
+                i--;
+                
+            }
+            else 
+            {
+                pass[i]=key;
+                i++;
+                printf("*");
+            }
+        }
+            if(strlen(pass) >= 7) {
+
+                printf("\npleas enter your pass again:\n");
+                key='\0';
+                i=0;
+                while(key != '\r' && key != ' ')
+                {
+                    
+                    key = _getch();
+                    if(key == '\b' && i>0)
+                    {
+                        i--;
+                        confirmpass[i] = '\0';
+                        printf("\b");
+                    }
+                    else 
+                    {
+                        confirmpass[i]=key;
+                        i++;
+                        printf("*");
+                    }
+                }
+                    if(strcmp(pass,confirmpass) != 0 )
+                    {
+                        //khoroj
+                        continue;
+                    }
+            }            
+            else
+            {
+                printf("\n");
+                continue;
+            }
+        break;
+    }
+    system("cls");
+    printf("pleas enter your email:\n");
+    scanf("%s",email);    
+    ptemp->pnext = (struct lasami* )malloc(sizeof(struct lasami));
+    ptemp=ptemp->pnext;
+    ptemp->pnext=NULL;
+    strcpy(ptemp->info.username ,name);
+    strcpy(ptemp->info.username ,pass);
+    strcpy(ptemp->info.username ,email);
+    ID++;
+    ptemp->info.ID = ID + maxId;
+    ptemp->info.win = 0;
+    ptemp->info.lose = 0;
+    return ptemp;
+}
+}
+ 
+struct lasami* sign_in(struct lasami* phead) {
+    char name[40];
+    char email[100];
+    char pass[20];
+    int input = 0;
+    char key='a';
+    char confirmpass[20];
+    int i =0;
+    struct lasami* ptemp;
+    while(1){
+        ptemp = phead ;
+        printf("enter your name:\n");
+        scanf("%s",name); 
+        while(1)
+        {
+
+            if(strcmp(ptemp->info.username , name ) == 0 ) {
+                while(1){
+                    printf("enter your pass:\n");
+                    scanf("%s",pass);
+                    if (strcmp(ptemp->info.password , pass ) == 0){
+                        ///game menu 
+                        //return ;
+                    }
+                    else {
+                        while(1){
+                            printf("enter 1 : try again\n enter 2 : forgot pass");
+                            if(input==2) {
+                                printf("enter your email:\n");
+                                scanf("%s",email);
+                                if(strcmp(ptemp->info.email , email) == 0){
+                                    while(1)
+                                    {
+                                        printf("enter your new pass\n");
+                                        i = 0;
+                                        pass[i] = '\0';
+                                        while(key != '\r' && key != ' ')
+                                        {
+                                            key = _getch();
+                                            if(key == '\b')
+                                            {
+                                                pass[i] = '\0';
+                                                i--;
+                                                printf("\b");
+                                            }
+                                            else 
+                                            {
+                                                pass[i]=key;
+                                                i++;
+                                                printf("*");
+                                            }
+                                            if(strlen(pass) >= 8){
+                                                key='\0';
+                                                while(key != '\r' && key != ' ')
+                                                {                                                    
+                                                    key = _getch();
+                                                    if(key == '\b')
+                                                    {
+                                                        confirmpass[i] = '\0';
+                                                        i--;
+                                                        printf("\b");
+                                                    }
+                                                    else 
+                                                    {
+                                                        confirmpass[i]=key;
+                                                        i++;
+                                                        printf("*");
+                                                    }
+                                                }
+                                                if(strcmp(pass,confirmpass) != 0 )
+                                                {
+                                                    continue;
+                                                }
+                                                strcpy(ptemp->info.password,pass);
+
+                                            }
+                                            else{
+                                                break;
+                                            }
+                                        }
+                                    }                                          
+                                }
+                            else {
+                                continue;
+                            }                                
+                            }
+                            else if(input==1) {
+                                break;
+                            }
+                        }                    
+                    }     
+                                                            
+                }
+            }    
+            else {
+                while(1){
+                    printf("enter 1 : try again\nenter 2 : exit\n");
+                    scanf("%d",&input);                    
+                    if(input==1){
+                        break;
+                    }
+                    if(input==2){
+                        //exit();
+                        //return ;
+                    }
+                }                                                   
+            }
+
+        if(ptemp->pnext)
+        {
+            break;
+        }
+        ptemp = ptemp->pnext;
+    }
+        return ptemp;
+    }
+}
+
